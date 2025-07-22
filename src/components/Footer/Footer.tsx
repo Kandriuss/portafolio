d0275d15bab9d953528eb697b1ad462d0e2ba28a
Kandriuss <andres.bolivar.sam@gmail.com>
2025-07-22 19:22:11 -0400

    Navegación en el footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,69 +1,22 @@
 import React from 'react';
 import styles from './Footer.module.css';
-import { 
-  FaGithub, 
-  FaLinkedin, 
-  FaInstagram, 
-  FaFacebook,
-  FaDiscord
-} from 'react-icons/fa';
 import '../../styles/global.css';
-import type { SocialLink } from '../../interface/SocialLink';
+import { PATHS } from '../../libs/types';
 
 const Footer: React.FC = () => {
-  
-  const socialLinks: SocialLink[] = [
-    {
-      name: 'GitHub',
-      url: 'https://github.com/tu-usuario',
-      icon: <FaGithub />
-    },
-    {
-      name: 'LinkedIn',
-      url: 'www.linkedin.com/in/andresbolviar-sam',
-      icon: <FaLinkedin />
-    },
-    {
-      name: 'Instagram',
-      url: 'https://instagram.com/tu-usuario',
-      icon: <FaInstagram />
-    },
-    {
-      name: 'Facebook',
-      url: 'https://facebook.com/tu-usuario',
-      icon: <FaFacebook />
-    },
-    {
-      name: 'Discord',
-      url: 'https://discord.gg/tu-servidor',
-      icon: <FaDiscord />
-    }
-  ];
-
-  const handleSocialClick = (url: string) => {
-    window.open(url, '_blank', 'noopener,noreferrer');
-  };
-
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.content}>
-          <div className={styles.socialSection}>
-            <h3 className={`${styles.socialTitle} title-bold`}>Encuentranos en nuestras redes sociales</h3>
-            <div className={styles.socialLinks}>
-              {socialLinks.map((link) => (
-                <button
-                  key={link.name}
-                  className={styles.socialButton}
-                  onClick={() => handleSocialClick(link.url)}
-                  aria-label={`Visitar ${link.name}`}
-                  title={link.name}
-                >
-                  {link.icon}
-                </button>
-              ))}
-            </div>
-          </div>
+          {/* NavBar del footer */}
+          <nav className={styles.footerNav}>
+            <ul className={styles.footerNavLinks}>
+              <li><a className="text" href={PATHS.HOME}>Inicio</a></li>
+              <li><a className="text" href={PATHS.ABOUT_ME}>Sobre Mi</a></li>
+              <li><a className="text" href={PATHS.PROJECTS}>Proyectos</a></li>
+              <li><a className="text" href={PATHS.SERVICES}>Servicios</a></li>
+            </ul>
+          </nav>
           
           <div className={styles.copyrightSection}>
             <p className={`${styles.copyright} text`}>
